ff751a6bb7c1c2c5a8c3b883e45dc1a3f78a4c43
Derrek Grecia <142808679+StupiD3r@users.noreply.github.com>
2024-11-21 10:26:12 +0800

    Add files via upload

---
+++ Nifer/src/main/java/frm/Main.java
@@ -0,0 +1,36 @@
+package frm;
+
+import Classes.Student;  // Import the Student class
+import Classes.Subject;
+import Classes.Enrollment;
+import com.mongodb.client.MongoDatabase;
+import java.util.List;    // Import List class for storing students
+
+public class Main {
+    public static void main(String[] args) {
+        // Connect to the MongoDB database
+        MongoDatabase database = Student.connectToDatabase();
+
+        // Fetch all students from the database
+        List<Student> students = Student.getAllStudentsFromDB(database); // Updated method name
+
+        // Print the fetched students
+        Student.printStudents(students);
+        
+        MongoDatabase database1 = Subject.connectToDatabase();
+
+        // Fetch all students from the database
+        List<Subject> subjects = Subject.getAllSubjectsFromDB(database1); // Updated method name
+
+        // Print the fetched students
+        Subject.printSubjects(subjects);
+        
+        MongoDatabase database2 = Enrollment.connectToDatabase();
+
+        // Fetch all students from the database
+        List<Enrollment> enrollments = Enrollment.getAllEnrollmentFromDB(database2); // Updated method name
+
+        // Print the fetched students
+        Enrollment.printEnrollments(enrollments);
+    }
+}
